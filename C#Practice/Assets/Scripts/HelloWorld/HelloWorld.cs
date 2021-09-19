f415e75e3d5dd74d2f7a37378abb1727c60e00a3
James <jp187238@falmouth.ac.uk>
2021-09-19 12:27:14 +0100

    Version 0.0.1 Created the project and added the Hello World function

---
+++ C#Practice/Assets/Scripts/HelloWorld/HelloWorld.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class HelloWorld : MonoBehaviour
+{
+   public Text Textfield;
+   public void SetText(string text){
+       Textfield.text = text;
+   }
+}
